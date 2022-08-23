6e86c0b709603fabd81184a93fecd714f7728baa
vanvietan <vanvietan2911@gmail.com>
2022-08-23 13:48:59 +0700

    day7-user

---
+++ internal/repository/user/update_user.go
@@ -0,0 +1,7 @@
+package user
+
+// UpdateUser: update different field of user
+// func (i impl) UpdateUser(Ctx context.Context, user *model.Users, userID int64) (model.Users, error) {
+// 	var tx *gorm.DB
+
+// }
