8eb039c8625bd2a8911ecac9df8fb411bedfa588
vanvietan <vanvietan2911@gmail.com>
2022-08-26 08:46:27 +0700

    day9

--- internal/handler/user/create_user.go
+++ internal/handler/user/create_user.go
@@ -16,11 +16,11 @@ func (h UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 
 func checkValidate(r *http.Request) (model.Users, error) {
 	userCheck, err := ioutil.ReadAll(r.Body)
-	if err != nil{
+	if err != nil {
 		return model.Users{}, errors.New("user invalid field")
 	}
 	bodyUser := bytes.NewReader(userCheck)
 
-	bodyUser.
-	
+	bodyUser.ReadAt()
+
 }
