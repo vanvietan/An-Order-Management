6fa28ce7785d3f7b189cb11ea69f13b972b542de
vanvietan <vanvietan2911@gmail.com>
2022-09-28 16:25:49 +0700

    day22

--- internal/handler/order/get_orders_response.go
+++ internal/handler/order/get_orders_response.go
@@ -1,35 +1,43 @@
 package order
 
-import "order-mg/internal/model"
+import (
+	"order-mg/internal/model"
+)
 
-type getOrdersResponse struct {
-	Orders []model.Order `json:"orders"`
-	Cursor int64         `json:"cursor"`
+func modelToResponseArray(orders []model.Order) []AnOrderResponse {
+	if len(orders) == 0 {
+		return nil
+	}
+	resp := make([]AnOrderResponse, len(orders))
+	for i, s := range orders {
+		resp[i].Id = s.Id
+		resp[i].Name = s.Name
+		resp[i].Description = s.Description
+		resp[i].TotalPrice = s.TotalPrice
+		resp[i].Quantity = s.Quantity
+		resp[i].Discount = s.Discount
+		resp[i].Shipping = s.Shipping
+		resp[i].Status = s.Status
+		resp[i].UserId = s.UserId
+		resp[i].DatePurchased = s.DatePurchased
+		resp[i].CreatedAt = s.CreatedAt
+		resp[i].UpdatedAt = s.UpdatedAt
+		resp[i].Histories = s.Histories
+	}
+	return resp
 }
 
-func toGetOrdersResponse(orders []model.Order) getOrdersResponse {
-	if len(orders) == 0 {
+type getOrdersResponse struct {
+	Orders []AnOrderResponse `json:"orders"`
+	Cursor int64             `json:"cursor"`
+}
+
+func toGetOrdersResponse(resp []AnOrderResponse) getOrdersResponse {
+	if len(resp) == 0 {
 		return getOrdersResponse{}
 	}
 	return getOrdersResponse{
-		Orders: orders,
-		Cursor: orders[len(orders)-1].Id,
+		Orders: resp,
+		Cursor: resp[len(resp)-1].Id,
 	}
 }
-
-//type getOrdersResponseB struct {
-//	Orders []AnOrderResponse `json:"orders"`
-//	Cursor int64             `json:"cursor"`
-//}
-//
-//func toGetOrdersResponseB(orders []model.Order) getOrdersResponseB {
-//	if len(orders) == 0 {
-//		return getOrdersResponseB{}
-//	}
-//	var res []AnOrderResponse
-//
-//	return getOrdersResponseB{
-//		Orders: res,
-//		Cursor: res[len(res)-1].Id,
-//	}
-//}
